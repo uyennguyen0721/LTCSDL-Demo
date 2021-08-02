498fdcd62aa9361313815a87e2049cd591bf4b5f
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-08-02 11:01:23 +0700

    update

---
+++ LTCSDL-IT81-LINQ/PetOwner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTCSDL_IT81_LINQ
+{
+    public class PetOwner
+    {
+        public string Name { get; set; }
+        public List<String> Pets { get; set; }
+
+    }
+}
--- LTCSDL-IT81-LINQ/Program.cs
+++ LTCSDL-IT81-LINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -52,6 +53,20 @@ namespace LTCSDL_IT81_LINQ
 			foreach (string s in subset)
 				Console.WriteLine("Item: {0}", s);
 		}
+
+		//Slide 47
+		static PetOwner[] GetPetList()
+		{
+			PetOwner[] petOwners =
+				{ new PetOwner { Name="Bảo",
+			  Pets = new List<string>{ "Chó", "Mèo" } },
+		  new PetOwner { Name="Thảo",
+			  Pets = new List<string>{ "Két", "Cá sấu" } },
+		  new PetOwner { Name="Tâm",
+			  Pets = new List<string>{ "Chuột", "Chó" } } };
+			return petOwners;
+		}
+
 
 
 
@@ -227,7 +242,7 @@ namespace LTCSDL_IT81_LINQ
 				Console.WriteLine(item.ToString());
 			}
 
-			/*
+            /*
 			//Slide 43
 			Console.WriteLine("The overstock items!");
 
@@ -241,14 +256,302 @@ namespace LTCSDL_IT81_LINQ
 			}
 			*/
 
-			//Slide 44
-			Console.WriteLine("The overstock items!");
+            //Slide 44
+            Console.WriteLine("The overstock items!");
 
-			var overstock = products.Where(p => p.NumberInStock > 25);
+            var overstock = products.Where(p => p.NumberInStock > 25);
 
-			foreach (Products c in overstock)
+            foreach (Products c in overstock)
+            {
+                Console.WriteLine(c.ToString());
+            }
+
+
+            // Slide 45
+            //Extract the ints from the ArrayList.
+            ArrayList myStuff = new ArrayList();
+            myStuff.AddRange(new object[] { 10, 400, 8, false, new Car(), "string data" });
+
+            var myInts = myStuff.OfType<int>();
+
+            // Prints out 10, 400, and 8.
+            foreach (int i in myInts)
+            {
+                Console.WriteLine("Int value: {0}", i);
+            }
+
+            //Slide 46
+            ArrayList myCars1 = new ArrayList()
+            {
+                new Car{ PetName = "Xe1", Color = "Bạc", Speed = 100, Make = "BMW"},
+                new Car{ PetName = "Xe2", Color = "Đen", Speed = 55, Make = "Toyota"},
+                new Car{ PetName = "Xe3", Color = "Xanh", Speed = 5, Make = "Honda"},
+                new Car{ PetName = "X4", Color = "Trắng", Speed = 43, Make = "Ford"}
+            };
+            // Transform ArrayList into an IEnumerable<T> compatible type.
+            var myCarsEnum = myCars1.OfType<Car>();
+            var fastCars = from c in myCarsEnum
+                           where c.Speed > 55
+                           select c;
+            foreach (var car in fastCars)
+            {
+                Console.WriteLine("{0} is going too fast!", car.PetName);
+            }
+
+            //Slide 47
+            PetOwner[] petList = GetPetList();
+
+            var query1 = from O in petList
+                         from p in O.Pets
+                         select p;
+
+            foreach (var p in query1)
+            {
+                Console.WriteLine(p);
+            }
+
+            //Slide 49
+            IEnumerable<string> query2 = petList.SelectMany(petOwner => petOwner.Pets);
+
+            Console.WriteLine("Using SelectMany():");
+
+            foreach (string pet in query2)
+            {
+                Console.WriteLine(pet);
+            }
+
+            //Slide 50
+            IEnumerable<List<String>> query3 = petList.Select(petOwner => petOwner.Pets);
+
+            // Notice that two foreach loops are required to 
+            // iterate through the results
+            // because the query returns a collection of arrays.
+            foreach (List<String> petlist in query3)
+            {
+                foreach (string pet in petlist)
+                {
+                    Console.WriteLine(pet);
+                }
+                Console.WriteLine();
+            }
+
+            //Slide 51
+            var subset = from p in products
+						 orderby p.ProductName descending
+						 select p;
+
+			Console.WriteLine("Ordered by Name:");
+			foreach (var p in subset)
 			{
-				Console.WriteLine(c.ToString());
+				Console.WriteLine(p.ToString());
+			}
+
+			//Slide 52
+			var subset1 = products.OrderBy(p => p.ProductName);
+			//var subset = 
+			products.OrderByDescending(p => p.ProductName);
+
+			Console.WriteLine("Ordered by Name:");
+			foreach (var p in subset1)
+			{
+				Console.WriteLine(p.ToString());
+			}
+
+			//SLide 53
+			var subset3 = products.OrderBy(p => p.ProductName).ThenBy(p => p.ProductId);
+			//var subset = products.OrderByDescending(p => p.ProductName);
+
+			Console.WriteLine("Ordered by Name:");
+			foreach (var p in subset3)
+			{
+				Console.WriteLine(p.ToString());
+			}
+
+			//Slide 54
+			var query4 = from p in products
+						group p by p.CategoryId;
+
+			foreach (var category in query4)
+			{
+				Console.WriteLine("CategoryId: {0}", category.Key);
+				foreach (Products item in category)
+				{
+					Console.WriteLine(item);
+				}
+			}
+
+			//SLide 55
+			var query5 = products.GroupBy(p => p.CategoryId);
+
+			foreach (var category in query5)
+			{
+				Console.WriteLine("CategoryId: {0}", category.Key);
+				foreach (Products item in category)
+				{
+					Console.WriteLine(item);
+				}
+			}
+
+			//Slide 56
+			var query6 = from p in products
+						join c in categories
+						on p.CategoryId equals c.CategoryId
+						group p by new
+						{
+							c.CategoryId,
+							c.CategoryName
+						};
+
+			foreach (var category in query6)
+			{
+				Console.WriteLine(category.Key);
+				foreach (Products item in category)
+				{
+					Console.WriteLine(item);
+				}
+			}
+
+			//Slide 57
+			var query7 = from p in products
+						group p by p.CategoryId into g
+						select new
+						{
+							Catgory = g.Key,
+							Prods = g
+						};
+
+			foreach (var item in query7)
+			{
+				Console.WriteLine(item.Catgory);
+				foreach (Products p in item.Prods)
+				{
+					Console.WriteLine(p);
+				}
+			}
+
+			//SLide 58
+			var query8 = products.GroupBy(p => p.CategoryId, p => p);
+
+
+			foreach (var item in query8)
+			{
+				Console.WriteLine(item.Key);
+				foreach (Products pa in item)
+				{
+					Console.WriteLine(pa);
+				}
+			}
+
+			//Slide 59
+			string[] strings =
+			{
+				"A penny saved is a penny earned.",
+				"The early bird catches the worm.",
+			};
+
+			var earlyBirdQuery =
+				from sentence in strings
+				let words = sentence.Split(' ')
+				from word in words
+				let w = word.ToLower()
+				where w[0] == 'a' || w[0] == 'e' || w[0] == 'i'
+					|| w[0] == 'o' || w[0] == 'u'
+				select word;
+
+			foreach (var v in earlyBirdQuery)
+			{
+				Console.WriteLine("\"{0}\" starts with a vowel", v);
+			}
+
+			//Slide 60
+			var query9 = from c in categories
+						join p in products
+						on c.CategoryId equals p.CategoryId
+						select new
+						{
+							p.ProductId,
+							p.ProductName
+						};
+
+			foreach (var item in query9)
+			{
+				Console.WriteLine(item);
+			}
+
+			//Slide 61
+			var query10 = categories.Join(products, c => c.CategoryId, p => p.CategoryId, (c, p) => new
+			{
+				p.ProductId,
+				p.ProductName,
+				c.CategoryId
+			});
+
+			foreach (var item in query10)
+			{
+				Console.WriteLine(item);
+			}
+
+			//SLide 64
+			var allProducts1 = from p in products
+							  select p;
+
+			foreach (var prod in allProducts1.Reverse())
+			{
+				Console.WriteLine(prod.ToString());
+			}
+
+			//Slide 65
+			List<string> myCars2 = new List<String> { "Ford", "Honda", "Toyota" };
+			List<string> yourCars =
+				new List<String> { "BMW", "Toyota", "Hyundai" };
+
+			var carConcat = (from c in myCars2 select c).
+				Concat(from c2 in yourCars select c2);
+
+			foreach (string s in carConcat.Distinct())
+				Console.WriteLine(s);
+
+			//SLide 66
+			var carUnion = (from c in myCars2 select c).
+				Union(from c2 in yourCars select c2);
+
+			Console.WriteLine("Here is everything:");
+			foreach (string s in carUnion)
+				Console.WriteLine(s);
+
+			//SLide 67
+			var carIntersect = (from c in myCars2 select c).
+			Intersect(from c2 in yourCars select c2);
+
+			Console.WriteLine("Here is what we have in common: ");
+			foreach (string s in carIntersect)
+				Console.WriteLine(s); // Printa Toyota.
+
+			//Slide 68
+			var carDiff = (from c in myCars2 select c).
+			Except(from c2 in yourCars select c2);
+
+			Console.WriteLine("Here is what you don’t have, but I do:");
+			foreach (string s in carDiff)
+				Console.WriteLine(s);
+
+			//Slide 69
+			List<int> nums1 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			var query11 = (from n in nums
+						 select n).Take(3);
+
+			foreach (int num in query11)
+			{
+				Console.WriteLine(num);
+			}
+
+			//SLide 70
+			var query12 = (from n in nums
+						 select n).Take(3);
+
+			foreach (int num in query12)
+			{
+				Console.WriteLine(num);
 			}
 
 
@@ -256,5 +559,15 @@ namespace LTCSDL_IT81_LINQ
 
 
 		}
+
+
+
+
+
+
+
+
+
 	}
 }
+
